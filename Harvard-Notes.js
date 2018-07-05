400b84dc9430daaad9800a79959f1e04c534c7ce
linazoo <linazooper@gmail.com>
2018-07-05 11:46:14 -0500

    typecasting coercion explicit vs implicit

--- Harvard-Notes.js
+++ Harvard-Notes.js
@@ -17,3 +17,11 @@ for (let i = 0; i < arr.length; i++) {
 
 // >> hi string 42 [Function]
 
+// TYPES: 
+// dynamic typing, primitive types (no methods, immutable): undefined, null, boolean, number, string, (synbol), everything else is an Object
+
+// Typecasting? Coercion
+// Explicit vs Implicit coercion
+const x = 42;
+const explicit = String(x); // >>> explicit === "42" //im just gonna tell you exactly what i want by wrapping it with the type 
+const implicit = x + ""; // >>> implicit === "42" // I'm going to rely on the behaviour of javascript to convert 
