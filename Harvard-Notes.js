89ece7c8a3c0f5a14fe246e9b2ee0171983ac241
linazoo <linazooper@gmail.com>
2018-07-05 16:43:48 -0500

    Object mutation

--- Harvard-Notes.js
+++ Harvard-Notes.js
@@ -25,3 +25,81 @@ for (let i = 0; i < arr.length; i++) {
 const x = 42;
 const explicit = String(x); // >>> explicit === "42" //im just gonna tell you exactly what i want by wrapping it with the type 
 const implicit = x + ""; // >>> implicit === "42" // I'm going to rely on the behaviour of javascript to convert 
+
+// types
+
+const x = 42; 
+// what type is this?
+console.log(typeof x);
+// caviat >>> typeof null >>> "object" 
+
+// when to use == vs === ?
+// never use == because you have to know how every single thing cohorses
+
+// coercion cont
+// which values are falsy? NaN, +0 -0, false, undefined, null, ""
+//which values are truthy? all numbers expect 0, {}, [], everthing else 
+
+// Objects
+// Objects, Arrays, Functions, Objects 
+// Everything else is an object
+
+// Primitives  vs Objects
+// Primitives are immutable: if you want to change them you are REPLACING THEM with a new value
+
+// Object LIteral PREFERED way 
+const o2 = {} 
+o2.firstName = "jordan";
+o2['lastName'] = "Hayashi";
+const key = "isTeaching"
+o[key] = true; 
+o['gree'] = function() {
+  console.log('hi');
+}
+
+// Creating an object
+const o = new Object();
+
+// now o is a new object thing
+o.firstNmae = "Jordan";
+o.lastName = 'Hayashi';
+o.isTeaching = true;
+o.greet = function () {
+  console.log('hi');
+}
+
+// put everything in line
+
+const o3 = {
+  firstName = "jordan",
+  lastName: "hayashi", 
+  isTeaching: true,
+  greet: function() {
+    console.log('hi');
+  }, // nest objects (object within an object)
+  address: {
+    street: 'Main st', 
+    number: 123,
+  }
+}
+
+// how do we get the values back out?
+// o3.address.number >>> 123 
+
+// Ojbect Mutation
+const o = {
+  a: 'a',
+  b: 'b',
+}
+
+// i wanna change a to be something else
+const o2 = o;
+o.a = 'new value'
+
+// console.log(o2.a); >>  new value 
+// stored by reference rather than value  - they are stored as rferences 
+
+// another way
+const o2 = Object.assign({}, o)
+// this is saying give me a brand new object and then merge the keys and values of the object called o
+
